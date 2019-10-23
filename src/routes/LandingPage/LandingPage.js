b63cfa5b93e54bd6c077b1988059e6cf24456c82
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-10-23 08:48:48 -0700

    style landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import './LandingPage.css';
 
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <>
-        <section>
-          <header>
-            <h3 class>Add family members</h3>
-          </header>
-          <p>[<em>placeholder for screenshot of a form to add a new family member</em>]</p>
-          <p>Origin helps you to keep track of your family history.</p>
+      <article>
+        <h2>Origin helps you to keep track of your family history.</h2>
+        <div className="landing">
+        <section className="how-to-use">
+          <h3 className="heading">Register an account</h3>
+          <img src="register.png" alt="Register button on the top right"></img>
         </section>
-        <section>
-          <header>
-            <h3>View your family tree</h3>
-          </header>
-          <p>[<em>placeholder for screenshot of a chart representing family relationships in a tree structure</em>]</p>
-          <p>Visualize your family tree. Find out what part of your family you might want to explore more.</p>
+        <section className="how-to-use">
+          <h3 className="heading">Click on your name to open your card</h3>
+          <img src="startadding.png" alt="Find add parents button"></img>
         </section>
-      </>
+        <section className="how-to-use">
+          <h3 className="heading">Click add parents button and add your ancestors</h3>
+          <img src="add.png" alt="Form to add your parents will open"></img>
+        </section>
+        <section className="how-to-use">
+          <h3 className="heading">Enjoy the site</h3>
+          <img src="tree.png" alt="Family tree view"></img>
+        </section>
+        </div>
+      </article>
     )
   }
 }
